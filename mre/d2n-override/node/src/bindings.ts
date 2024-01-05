af7ebd62a5133c7f2da04f40f55b0ba169f8d486
Lord-Valen <lord_valen@protonmail.com>
2024-01-05 13:27:05 -0500

    cleanup

--- mre/d2n-override/node/src/bindings.ts
+++
@@ -1,7 +0,0 @@
-import { AsyncContainerModule, interfaces } from "inversify";
-import { App } from "./App.js";
-import { IApp } from "./interfaces.js";
-
-export const bindings = new AsyncContainerModule(async (bind: interfaces.Bind) => {
-    bind(IApp).to(App);
-});
